81a27b8dfbcb0732236544dfab4d0a2084e9cc5a
@CleinKelvinn <77683874+MrX0955@users.noreply.github.com>
2024-06-29 02:57:23 +0300

    Updated June 29, 2024 at 3:00 PM ~ Made By CleinKelvinn

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-from pystyle    import Colors, Colorate
-from colorama   import Fore
+import os, subprocess
 from datetime   import datetime
 from rstr       import xeger
-from os         import system, name, path
+from pystyle    import Colors, Colorate
+from colorama   import Fore, init
 
-system('title "| Avast Key Generator | Developer: cleinkelvinn |"')
-system("cls" if name == "nt" else "clear")
+init(autoreset=True)
 
-class generate:
-    print(
-        Colorate.Horizontal(
-            Colors.purple_to_red,
-            """
+class AvastKeyGenerator:
+    def __init__(self):
+        self.key_pattern = (
+            r"MCT[79RBYN2GKZ][XK37BM4FP8Y]-[DZCXBY9NMKJ][NKZBQXR9M27P][HP8DMJRF6XB24][3826MX9K7]"
+            r"[BD3FKH2GX8Q7]-[73K28GRN6QJ][6HMG3Y82WCK][HCWFKJ7432D9][D9YXRGPHJK2][6ZT4FNBWQHXD2G]"
+            r"-[Y89G2Z3M7FQ][427P8ZNQ93X6][JE7KVDUWGQTZYC][KGUD6HFR98Y][NEP4UDBSVXQ]"
+        )
+        self.file_name = "keysAvast.txt"
+        self.setup_console()
+
+    def setup_console(self):
+        if os.name == "nt":
+            os.system('title "| Avast Key Generator | Creator: cleinkelvinn |"')
+        self.clear_console()
+
+    @staticmethod
+    def clear_console():
+        os.system("cls" if os.name == "nt" else "clear")
+
+    @staticmethod
+    def display_banner():
+        banner = """
                                         ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
                                         ░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░
                                         ░░░░░░░░▄▀░░░░░░░░░░░░▄░░░░░░░▀▄░░░░░░░
@@ -25,40 +41,65 @@ class generate:
                                         ░░░░░░░▄█▄░░░░░░░░░░░▀▄░░▀▀▀▀▀▀▀▀░░▄▀░░
                                         ░░░░░░█▀▀█████████▀▀▀▀████████████▀░░░░
                                         ░░░░░░████▀░░███▀░░░░░░▀███░░▀██▀░░░░░░
-                                        ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░""",
-            1,
-        )
-    )
-    now = datetime.now()
-    current_time = now.strftime("[%d/%m/%Y %H:%M:%S]")
-    limit = int(
-        input(
-            f"\n {Fore.LIGHTRED_EX}{current_time}{Fore.YELLOW} > "
-            + Colorate.Diagonal(
-                Colors.rainbow, "Kaç Tane Key Oluşturmak İstiyorsunuz: ", 2
-            )
-        )
-    )
-    umm = 0
-    while umm < limit:
-        mrx = xeger(
-            r"MCT[79RBYN2GKZ][XK37BM4FP8Y]-[DZCXBY9NMKJ][NKZBQXR9M27P][HP8DMJRF6XB24][3826MX9K7][BD3FKH2GX8Q7]-[73K28GRN6QJ][6HMG3Y82WCK][HCWFKJ7432D9][D9YXRGPHJK2][6ZT4FNBWQHXD2G]-[Y89G2Z3M7FQ][427P8ZNQ93X6][JE7KVDUWGQTZYC][KGUD6HFR98Y][NEP4UDBSVXQ]"
-        )
-        with open("keysAvast.txt", "a") as f:
-            if len(mrx) == 0:
-                exit("An error occured!")
-            f.writelines(mrx + "\n")
-            f.close()
-            umm += 1
-            if umm >= limit:
-                print(
-                    f"\n {Fore.LIGHTRED_EX}{current_time}{Fore.YELLOW} > "
-                    + Colorate.Diagonal(
-                        Colors.rainbow,
-                        "Key Oluşturma Tamamlandı, Keyler Kaydedildi.",
-                        4,
-                    )
-                )
-                if path.exists("keysAvast.txt"):
-                    system("start keysAvast.txt")
+                                        ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"""
+        print(Colorate.Horizontal(Colors.purple_to_red, banner, 1))
+
+    @staticmethod
+    def get_key_count():
+        now = datetime.now().strftime("[%d/%m/%Y %H:%M:%S]")
+        while True:
+            try:
+                count = int(input(f"\n {Fore.LIGHTRED_EX}{now}{Fore.YELLOW} > "
+                                  + Colorate.Diagonal(Colors.rainbow, 
+                                                      "Enter Key Amount: ", 2)))
+                return count
+            except ValueError:
+                print(Fore.RED + "\n 🔮 Please Select a Number!")
+
+    def generate_keys(self, count):
+        with open(self.file_name, "a") as file:
+            for _ in range(count):
+                key = xeger(self.key_pattern)
+                file.write(key + "\n")
+        return self.file_name
+
+    def notify_completion(self):
+        now = datetime.now().strftime("[%d/%m/%Y %H:%M:%S]")
+        print(f"\n {Fore.LIGHTRED_EX}{now}{Fore.YELLOW} > "
+              + Colorate.Diagonal(Colors.rainbow, "Keys Has Been Created!", 4))
+
+    def open_file(self):
+        now = datetime.now().strftime("[%d/%m/%Y %H:%M:%S]")
+        while True:
+            qi = input(f"\n {Fore.LIGHTRED_EX}{now}{Fore.YELLOW} > "
+                  + Colorate.Diagonal(Colors.rainbow, "Do you want to open saved key file?: (y/n) ", 4)).lower()
+            if qi in ["y", "n"]:
                 break
+            print(Fore.RED + "\n 🔮 Please enter 'y' or 'n'!")
+
+        if qi == "y":
+            if os.path.exists(self.file_name):
+                try:
+                    if os.name == "nt":
+                        os.startfile(self.file_name)
+                    else:
+                        subprocess.call(('xdg-open', self.file_name))
+                except Exception as e:
+                    print(Fore.RED + f"\n 🔮 Error opening file: {e}")
+            else:
+                print(Fore.RED + "\n 🔮 File does not exist!")
+        else:
+            exit()
+
+def main():
+    generator = AvastKeyGenerator()
+    generator.display_banner()
+    count = generator.get_key_count()
+    generator.generate_keys(count)
+    generator.notify_completion()
+    generator.open_file()
+
+if __name__ == "__main__":
+    main()
+
+# Updated June 29, 2024 at 3:00 PM ~ Made By CleinKelvinn
